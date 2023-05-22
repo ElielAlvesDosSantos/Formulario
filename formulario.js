9f3b969b50f65bc480e9061c4e381587c129f366
ELIEL ALVES DOS SANTOS <eliel_a_santos@estudante.sesisenai.org.br>
2023-05-22 08:49:19 -0300

    first commit

---
+++ formulario.js
@@ -0,0 +1,11 @@
+let teste = document.getElementById("teste");
+// console.log(teste)
+let btn_enviar = document.querySelector("#btn_enviar");
+let res = document.querySelector("#res");
+
+btn_enviar.addEventListener("click", () => {
+  let produto = document.querySelector("#produto").value;
+  let qtde = document.querySelector("#qtde").value;
+  res.innerHTML =
+    "<br>" + "produto = " + produto + " e quantidade = " + qtde + "kg";
+});
